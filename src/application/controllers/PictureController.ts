747b35ed178e826d95c86c69b8b16513c9a94cac
Furkan <furkankocyigit58@gmail.com>
2023-11-16 19:17:03 +0300

    picture-to-buffer service is implemented

--- src/application/controllers/PictureController.ts
+++ src/application/controllers/PictureController.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import sharp from 'sharp';
-const { get } = require('request-promise');
+
 import 'dotenv/config';
-import { IPictureGeneratorService, IPostService } from '../../domain.services';
+import { IPictureGeneratorService, IPictureToBufferService, IPostService } from '../../domain.services';
 import { SERVICES } from '../../config/identifiers';
 import { inject, injectable } from 'inversify';
 
@@ -10,25 +9,26 @@ import { inject, injectable } from 'inversify';
 export class PictureController {
     private pictureGeneratorService: IPictureGeneratorService;
     private postService: IPostService;
+    private pictureToBufferService: IPictureToBufferService;
 
     constructor(
         @inject(SERVICES.PictureGeneratorService) pictureGeneratorService: IPictureGeneratorService,
-        @inject(SERVICES.PostService) postService: IPostService
+        @inject(SERVICES.PostService) postService: IPostService,
+        @inject(SERVICES.PictureToBufferService) pictureToBufferService: IPictureToBufferService
     ) {
         this.pictureGeneratorService = pictureGeneratorService;
         this.postService = postService;
+        this.pictureToBufferService = pictureToBufferService;
     }
 
     postPicture = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { creationText } = req.body;
+
             const pictureUrl = await this.pictureGeneratorService.generatePictureUrl(creationText);
-            const imageBuffer = await get({
-                url: pictureUrl,
-                encoding: null,
-            });
-            const resizedPictureBuffer = await sharp(imageBuffer).resize(1696, 1064).jpeg().toBuffer();
-            const postID = await this.postService.postPicture(resizedPictureBuffer, creationText);
+            const PictureBuffer = await this.pictureToBufferService.pictureToBuffer(pictureUrl);
+            const postID = await this.postService.postPicture(PictureBuffer, creationText);
+
             res.send('picture is shared with id: ' + postID + ' url: ' + pictureUrl);
         } catch (err) {
             next(err);
