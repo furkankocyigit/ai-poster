b6baee02c89a86355873b7ab43ccf34f61bfccaa
Furkan <furkankocyigit58@gmail.com>
2023-11-17 17:17:11 +0300

    model is changed

--- src/application/controllers/ChatController.ts
+++ src/application/controllers/ChatController.ts
@@ -3,10 +3,18 @@ import { inject, injectable } from 'inversify';
 import OpenAI from 'openai';
 import { INSTANCES } from '../../config/identifiers';
 
+// Possible models to use:
+// gpt-4-1106-preview   ----> latest gpt4 model with 128,000 tokens and up to date to Apr 2023
+// gpt-4-vision-preview ----> latest gpt4 model with visiual input with 128,000 tokens and up to date to Apr 2023
+// gpt-4                ----> 8,192 tokens	Up to Sep 2021
+// gpt-3.5-turbo-1106   ----> The latest GPT-3.5 Turbo model with 16,385 tokens	Up to Sep 2021
+// gpt-3.5-turbo        ----> 4,096 tokens	Up to Sep 2021
+
+// TODO: Refactor this as not to use the OpenAI client directly in the controller, add service layer
 @injectable()
 export class ChatController {
     private openai: OpenAI;
-
+    private model = 'gpt-4-1106-preview';
     constructor(@inject(INSTANCES.OpenAIClient) openAIClient: OpenAI) {
         this.openai = openAIClient;
     }
@@ -17,7 +25,7 @@ export class ChatController {
 
             const gptResponse = await this.openai.chat.completions.create({
                 messages: [{ role: 'user', content: message }],
-                model: 'gpt-3.5-turbo',
+                model: this.model,
             });
 
             const responseString = gptResponse.choices[0].message;
